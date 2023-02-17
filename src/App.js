9b3403d673ae83916780cec4de18d512161c2117
Skrypnykov <oskripnikov@gmail.com>
2023-02-17 19:42:06 +0200

    #3: React Pizza v2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Header, Categories, Sort, PizzaBlock } from './components';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock title="Мексиканская" price={150} />
+              <PizzaBlock title="Пеперони" price={130} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
